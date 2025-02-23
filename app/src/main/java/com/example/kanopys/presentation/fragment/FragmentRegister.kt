1c7f8551a2c6dee5f10ed412466b2cceea503295
coffeek <berezkov.dima.16@mail.ru>
2025-02-24 01:05:40 +0500

    add firebase authentication

--- app/src/main/java/com/example/kanopys/presentation/fragment/FragmentRegister.kt
+++ app/src/main/java/com/example/kanopys/presentation/fragment/FragmentRegister.kt
@@ -2,6 +2,8 @@ package com.example.kanopys.presentation.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +11,11 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.example.kanopys.R
 import com.example.kanopys.databinding.FragmentRegisterBinding
 import com.example.kanopys.presentation.KanopysApplication
 import com.example.kanopys.presentation.viewmodel.RegisterViewModel
 import com.example.kanopys.presentation.viewmodel.ViewModelFactory
-import io.ktor.server.engine.embeddedServer
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -50,6 +52,40 @@ class FragmentRegister : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
+        changingTheInputDataUser()
+    }
+
+    private fun changingTheInputDataUser() {
+        binding.edName.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorName()
+                binding.tilUserName.error = null
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        binding.edPassword.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorPassword()
+                binding.tilUserPassword.error = null
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        binding.edRepeatPassword.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorRepeatPassword()
+                binding.tilUserRepeatPassword.error = null
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     private fun observeViewModel() {
@@ -62,10 +98,12 @@ class FragmentRegister : Fragment() {
                     checkingTheEnteredData(name, password)
                 }
                 password != repeatPassword -> {
-                    Toast.makeText(
-                        requireContext(),
-                        "Пароли не совпадают!",
-                        Toast.LENGTH_SHORT).show()
+                    binding.tilUserPassword.apply {
+                        error = getString(R.string.passwords_not_match)
+                    }
+                    binding.tilUserRepeatPassword.apply {
+                        error = getString(R.string.passwords_not_match)
+                    }
                 }
             }
         }
@@ -79,10 +117,12 @@ class FragmentRegister : Fragment() {
                 }
             }
             name.isEmpty() && password.isEmpty() && repeatPassword.isEmpty() -> {
-                Toast.makeText(
-                    requireContext(),
-                    "Вы ничего не ввели в поля ввода!",
-                    Toast.LENGTH_SHORT).show()
+                viewModel.showError(name, password, repeatPassword)
+                binding.apply {
+                    tilUserName.error = getString(R.string.error_name)
+                    tilUserPassword.error = getString(R.string.error_password)
+                    tilUserRepeatPassword.error = getString(R.string.error_repeat_password)
+                }
             }
         }
     }
